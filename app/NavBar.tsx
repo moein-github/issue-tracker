44ba7459f599416c847f6e41d0605169dd2e1808
moein bannaye zahmati <moein.bannaye.zahmati@gmail.com>
2024-02-28 22:47:43 +0330

    style the issue details page

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,12 +5,12 @@ import classNames from "classnames";
 import { AiFillBug } from "react-icons/ai";
 import { usePathname } from "next/navigation";
 
-import content from "./content.json";
+import { navbar } from "./content.json";
 
 const NavBar = () => {
   const currentPath = usePathname();
 
-  const { links } = content.navbar;
+  const { links } = navbar;
 
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
